0ae22fc25b92cda0aad45f1b798521b881d4ef21
jisan06 <jisanahmed06@gmail.com>
2020-07-12 19:22:44 +0600

    Update 12-july-2020

--- resources/views/admin/index/marchant.blade.php
+++ resources/views/admin/index/marchant.blade.php
@@ -15,26 +15,99 @@
 	        color: #fff;
 	    }
 
+	    .top_card{
+	    	height: 130px;
+	    }
+
 	    .nav-tabs .nav-link.active, .nav-tabs .nav-item.show .nav-link {
 	       background: #00c292;
 	        color: #fff;
+	    }
+
+	    .first_block_title p{
+	    	color: #00c292;
+		    text-transform: uppercase;
+		    font-weight: bold;
+		    margin-bottom: 0px;
+		    margin-top: 7px;
+		    font-size: 12px;
+	    }
+
+	    .first_block_title a{
+	    	color: #00c292;
+    		font-size: 16px;
+	    }
+
+	    .second_block_title p{
+	    	color: #1f9a11;
+		    text-transform: uppercase;
+		    font-weight: bold;
+		    margin-bottom: 0px;
+		    margin-top: 7px;
+		    font-size: 12px;
+	    }
+
+	    .second_block_title a{
+	    	color: #1f9a11;
+    		font-size: 16px;
+	    }
+
+	    .third_block_title p{
+	    	color: #089faf;
+		    text-transform: uppercase;
+		    font-weight: bold;
+		    margin-bottom: 0px;
+		    margin-top: 7px;
+		    font-size: 12px;
+	    }
+
+	    .third_block_title a{
+	    	color: #089faf;
+    		font-size: 16px;
+	    }
+
+	    .fourth_block_title p{
+	    	color: #2329ea;
+		    text-transform: uppercase;
+		    font-weight: bold;
+		    margin-bottom: 0px;
+		    margin-top: 7px;
+		    font-size: 12px;
+	    }
+
+	    .fourth_block_title a{
+	    	color: #2329ea;
+    		font-size: 16px;
 	    }
 
 	</style>
 
 	<div class="row">
 		<div class="col-lg-3">
-			<div class="card">
+			<div class="card top_card">
 		        <div class="card-body">
-		            <div class="row">
-		                <div class="col-md-12">
+		        	<div class="row">
+		        		<h3>
+		        			<a href="{{ route('merchantBookingOrder.index') }}" style="font-size: unset;color: unset;">
+		        				<i class="fa fa-plus"></i>
+		        			</a>
+		        		</h3>
+		        	</div>
+		            <div class="row first_block_title">
+		                <div class="col-md-10">
 		                    <div class="d-flex no-block align-items-center">
 		                        <div>
-		                            <h3><i class="fa fa-plus"></i></h3>
-		                            <p class="text-muted">New Order</p>
+		                            <a href="{{ route('merchantBookingOrder.index') }}" style="font-size: unset;">
+		                            	<p>New Order</p>
+		                            </a>
 		                        </div>
+		                    </div>
+		                </div>
+
+		                <div class="col-md-2">
+		                	<div class="d-flex no-block align-items-center">
 		                        <div class="ml-auto">
-		                            <h2 class="counter text-primary"><a href="">1</a></h2>
+		                            <h3 class="counter text-primary"><a href="">{{count($new_order_list)}}</a></h3>
 		                        </div>
 		                    </div>
 		                </div>
@@ -44,23 +117,27 @@
 		</div>
 
 		<div class="col-lg-3">
-			<div class="card">
+			<div class="card top_card">
 		        <div class="card-body">
-		            <div class="row">
-		                <div class="col-md-12">
+		        	<div class="row">
+		        		<h3><i class="fa fa-refresh"></i></h3>
+		        	</div>
+		            <div class="row second_block_title">
+		                <div class="col-md-10">
 		                    <div class="d-flex no-block align-items-center">
 		                        <div>
-		                            <h3><i class="icon-note"></i></h3>
-		                            <p class="text-muted">Running Order</p>
-		                        </div>
-		                        <div class="ml-auto">
-		                            <h2 class="counter text-cyan"><a href=""></a></h2>
+		                            <p>Current Order</p>
+		                            <p>Processing Order</p>
 		                        </div>
 		                    </div>
 		                </div>
-		                <div class="col-12">
-		                    <div class="progress">
-		                        <div class="progress-bar bg-cyan" role="progressbar" style="width: 85%; height: 6px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+
+		                <div class="col-md-2">
+		                	<div class="d-flex no-block align-items-center">
+		                        <div class="ml-auto">
+		                            <h3 class="counter text-primary"><a href="">{{count($new_order_list) + count($running_order_list)}}</a></h3>
+		                            <h3 class="counter text-primary"><a href="">{{count($running_order_list)}}</a></h3>
+		                        </div>
 		                    </div>
 		                </div>
 		            </div>
@@ -69,23 +146,29 @@
 		</div>
 
 		<div class="col-lg-3">
-			<div class="card">
+			<div class="card top_card">
 		        <div class="card-body">
-		            <div class="row">
-		                <div class="col-md-12">
+		        	<div class="row">
+		        		<h3><i class="fa fa-toggle-on" aria-hidden="true"></i></i></h3>
+		        	</div>
+		            <div class="row third_block_title">
+		                <div class="col-md-10">
 		                    <div class="d-flex no-block align-items-center">
 		                        <div>
-		                            <h3><i class="icon-doc"></i></h3>
-		                            <p class="text-muted">Order Status</p>
-		                        </div>
-		                        <div class="ml-auto">
-		                            <h2 class="counter text-purple"><a href=""></a> </h2>
+		                            <p>Complete Order</p>
+		                            <p>Canceled Order</p>
 		                        </div>
 		                    </div>
 		                </div>
-		                <div class="col-12">
-		                    <div class="progress">
-		                        <div class="progress-bar bg-purple" role="progressbar" style="width: 85%; height: 6px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+
+		                <div class="col-md-2">
+		                	<div class="d-flex no-block align-items-center">
+		                        <div class="ml-auto">
+		                            <h3 class="counter text-primary">
+		                            	<a href="">{{count($complete_order_list)}}</a>
+		                            	<a href="">0</a>
+		                            </h3>
+		                        </div>
 		                    </div>
 		                </div>
 		            </div>
@@ -94,23 +177,27 @@
 		</div>
 
 		<div class="col-lg-3">
-			<div class="card">
+			<div class="card top_card">
 		        <div class="card-body">
-		            <div class="row">
-		                <div class="col-md-12">
+		        	<div class="row">
+		        		<h3><i class="fa fa-money"></i></h3>
+		        	</div>
+		            <div class="row fourth_block_title">
+		                <div class="col-md-10">
 		                    <div class="d-flex no-block align-items-center">
 		                        <div>
-		                            <h3><i class="icon-bag"></i></h3>
-		                            <p class="text-muted">Transaction</p>
-		                        </div>
-		                        <div class="ml-auto">
-		                            <h2 class="counter text-success"><a href=""></a></h2>
+		                            <p>Total Amount</p>
+		                            <p>Due Amount</p>
 		                        </div>
 		                    </div>
 		                </div>
-		                <div class="col-12">
-		                    <div class="progress">
-		                        <div class="progress-bar bg-success" role="progressbar" style="width: 85%; height: 6px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+
+		                <div class="col-md-2">
+		                	<div class="d-flex no-block align-items-center">
+		                        <div class="ml-auto">
+		                            <h3 class="counter text-primary"><a href="">1000</a></h3>
+		                            <h3 class="counter text-primary"><a href="">0</a></h3>
+		                        </div>
 		                    </div>
 		                </div>
 		            </div>
